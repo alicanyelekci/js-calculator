8b31e288781cec7be9c9aa4a6f3f3492ca4ed6c6
Ali Can Yelekci <acyelekci@gmail.com>
2022-06-21 21:55:28 +0300

    fixed updateDisplay

--- main.js
+++ main.js
@@ -85,9 +85,9 @@ class Calculator {
             if (this.operation != null){
                 this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
             }
-        }
-        else {
-            this.previousOperandTextElement.innerText = ''
+            else {
+                this.previousOperandTextElement.innerText = ''
+            }
         }
     }
 }
